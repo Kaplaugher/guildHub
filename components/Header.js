32f1ba8684f646cb77b11e9f0c356f1c367197ed
Kyle <kaplaugher94@gmail.com>
2021-05-21 13:54:51 -0500

    header done

--- components/Header.js
+++ components/Header.js
@@ -13,24 +13,47 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from '@heroicons/react/outline';
+import HeaderIcon from './HeaderIcon';
 
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* left */}
-      <div>
+      <div className="flex items-center ml-2 rounded-full">
         <Image
           src="https://links.papareact.com/5me"
           width={40}
           height={40}
           layout="fixed"
         />
-        <div>
-          <input type="text" placeholder="search GuildHub" />
+        <div className="flex bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            className="hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none flex-shrink"
+            type="text"
+            placeholder="search GuildHub"
+          />
         </div>
       </div>
       {/* center */}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
       {/* right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* profile pic */}
+        <p className="font-semibold whitespace-nowrap">Kyle Plaugher</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
     </div>
   );
 }
